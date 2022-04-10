683457a2436c9978ca97b21ac823c2faa7bc52cd
Kajol Roy <76867342+ikajolroy@users.noreply.github.com>
2022-04-10 20:30:04 +0600

    Update Cloudinary.js

--- backend/Helpers/Cloudinary.js
+++ backend/Helpers/Cloudinary.js
@@ -2,8 +2,8 @@ import cloud from 'cloudinary'
 const cloudinary = cloud.v2
 
 export default cloudinary.config({
-    cloud_name: 'sample',
-    api_key: '874837483274837',
-    api_secret: 'a676b67565c6767a6767d6767f676fe1',
+    cloud_name: 'Example',
+    api_key: '8748374832Example',
+    api_secret: 'a676b67565c6767a6767d6Example',
     secure: true
 });
